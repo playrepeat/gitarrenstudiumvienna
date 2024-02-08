c1c7fa8a5b699a80ce9f9ece3f511c243cfb9323
playrepeat <51443117+playrepeat@users.noreply.github.com>
2024-02-08 14:21:45 +0100

    bgimage etc

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -6,11 +6,11 @@ const pageController = {
   getIndex: (req, res) => {
     res.render('index.ejs', { messages: req.flash() });
   },
-  getVideos: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/videos'));
-  },
   getAbout: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/info'));
+    res.render('about.ejs' );
+  },
+  getVideos: (req, res) => {
+    res.render('videos.ejs' );
   },
   getContact: (req, res) => {
     req.flash('success', 'Email sent successfully!');
